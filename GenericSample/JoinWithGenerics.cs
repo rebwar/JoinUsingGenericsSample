639946fc7ab37596d3eb3546496b66990a12a1e7
Rebwar Hosain Poori <Rebwar.poori@hotmail.com>
2019-04-30 08:17:46 +0430

    Add project files.

---
+++ GenericSample/JoinWithGenerics.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GenericSample
+{
+    public class JoinWithGenerics<T>
+    {
+        public T JoinMethod(dynamic GenericParam1, dynamic GenericParam2)
+        {
+            var x = GenericParam1 + GenericParam2;
+            Console.WriteLine(x);
+            return x;
+        }
+    }
+}
---
+++ GenericSample/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GenericSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter String1:");
+            var str1 = Console.ReadLine();
+            Console.Write("Enter String2:");
+            var str2 = Console.ReadLine();
+            JoinWithGenerics<string> strGeneric = new JoinWithGenerics<string>();
+            strGeneric.JoinMethod(str1, str2);
+            Console.WriteLine("--------------------------------------------");
+            JoinWithGenerics<int> intGeneric = new JoinWithGenerics<int>();
+            intGeneric.JoinMethod(20, 30);
+            Console.ReadLine();
+        }
+    }
+}
